3048995de62370ea96634fd771c12230060364cb
hiheehee <gkek41@naver.com>
2021-08-02 23:27:48 +0900

    Add Define

---
+++ src/utils/Define.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Define {
+	public static final int KOREAN = 1001;  // 국어
+	public static final int MATH = 2001;    // 수학
+	public static final int ENGLISH = 3001; // 영어 
+		
+	public static final int AB_TYPE = 0;    // A+, A, B+, B, C+, C
+	public static final int SAB_TYPE = 1;   // S, A, B, c
+	public static final int PF_TYPE = 2;   // P, F
+}
