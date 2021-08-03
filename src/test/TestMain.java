eda79c465d9ee167886bdcea2d54f9713d8a5ec8
hiheehee <gkek41@naver.com>
2021-08-03 09:45:32 +0900

    Add EOF

--- src/test/TestMain.java
+++ src/test/TestMain.java
@@ -1,5 +1,9 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import school.School;
 import school.Score;
 import school.Student;
@@ -16,15 +20,63 @@ public class TestMain {
 	
 	GenerateGradeReport gradeReport = new GenerateGradeReport(); // 출력
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		TestMain test = new TestMain();
 		
 		test.creatSubject();
-		test.createStudent();
+		test.createTestStudent();
+		
+		System.out.print("추가하고 싶은 학생이 있습니까? (T/F) : ");
+		String str = br.readLine().toLowerCase();
+		while(str.equals("t")) {
+			System.out.print("학생의 학번을 입력하세요 : ");
+			int id = Integer.parseInt(br.readLine());
+			System.out.print("학생의 이름을 입력하세요 : ");
+			String name = br.readLine();
+			System.out.print("학생의 주전공을 입력하세요 : ");
+			String major = br.readLine().toLowerCase();
+			System.out.print("학생의 국어 점수를 입력하세요 : ");
+			int ks = Integer.parseInt(br.readLine());
+			System.out.print("학생의 수학 점수을 입력하세요 : ");
+			int ms = Integer.parseInt(br.readLine());
+			System.out.print("해당 학생은 기본영어를 수강습니까?(T/F) : ");
+			String check = br.readLine().toLowerCase();
+			int es = -1;
+			if (check.equals("t")) {
+				System.out.print("학생의 영어 점수을 입력하세요 : ");
+				es = Integer.parseInt(br.readLine());
+			}
+			
+			test.createStudent(id, name, major, ks, ms, es);
+
+			System.out.print("추가하고 싶은 학생이 있습니까? (T/F) : ");
+			str = br.readLine().toLowerCase();
+		}
 		
 		String report = test.gradeReport.getReport(); //성적 결과 생성
 		System.out.println(report); // 출력
 		
+	}
+	
+	public void createStudent(int id, String name, String major, int ks, int ms, int es){		
+		Subject maj = korean;;
+		if(major.equals("math")) maj = math;
+		
+		Student student = new Student(id, name, maj);
+		
+		happySchool.addStudent(student); // 학교에 학생 등록
+		
+		korean.register(student); // 국어 과목 수강신청	
+		addScoreForStudent(student, korean, ks); // 국어 과목 성적 등록 
+		
+		math.register(student); // 수학 과목 수강신청
+		addScoreForStudent(student, math, ms); // 수학과목 성적 등록
+		
+		if(-1 < es) {
+			english.register(student); // 영어 과목 수강신청
+			addScoreForStudent(student, english, es); // 영어 과목 성적 등록
+		}
 	}
 	
 	//테스트 과목 생성
@@ -42,7 +94,7 @@ public class TestMain {
 	}
 	
 	//테스트 학생 생성
-	public void createStudent(){
+	public void createTestStudent(){
 		Student student1 = new Student(150302, "Tomas", korean);
 		Student student2 = new Student(150408, "Jenny", math);
 		Student student3 = new Student(170201, "Eavan", korean);
@@ -93,7 +145,6 @@ public class TestMain {
 		addScoreForStudent(student1, english, 95);	
 		addScoreForStudent(student2, english, 85); 
 		addScoreForStudent(student3, english, 55);	
-		
 	}
 
 	//과목별 성적 입력
